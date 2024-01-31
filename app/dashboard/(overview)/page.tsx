1afbf7d142c74a63061e7143239bd3617530a0c1
Ni.Bagherpour <ni.bagherpour@sadad.co.ir>
2024-01-31 14:16:20 +0330

    feat: stream <RevenueChart/> component

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,15 +1,13 @@
+import { Suspense } from 'react';
+
 import { lusitana } from '@/app/ui/fonts';
-import {
-  fetchCardData,
-  fetchLatestInvoices,
-  fetchRevenue,
-} from '../../lib/data';
+import { fetchCardData, fetchLatestInvoices } from '../../lib/data';
 import { Card } from '../../ui/dashboard/cards';
 import LatestInvoices from '../../ui/dashboard/latest-invoices';
 import RevenueChart from '../../ui/dashboard/revenue-chart';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {
     totalPaidInvoices,
@@ -34,7 +32,9 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
